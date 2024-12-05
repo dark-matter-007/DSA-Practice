1ef6368a99b080791d392af5e8ea84f05c21e836
dark-matter-007 <ashwin.aksharma.p@outlook.com>
2024-12-05 23:38:47 +0530

    day 1

--- src/EvenOddSegregation.java
+++ src/EvenOddSegregation.java
@@ -1,2 +1,56 @@
 public class EvenOddSegregation {
+
+    public static void main(String[] argv) {
+        int[] arr = {6,5,4,3,2,1};
+        EvenOddSegregation evenOddSegregation = new EvenOddSegregation();
+        evenOddSegregation.segregateEvenOdd(arr);
+        for (int element : arr){
+            System.out.print(element + " ");
+        }
+    }
+
+    boolean isEven(int num){
+        return num % 2 == 0;
+    }
+
+    void sortArray(int[] arr) {
+        boolean found = true;
+        while (found) {
+            boolean innerfound = false;
+            for (int index = 1; index < arr.length; index++){
+                if(arr[index]<arr[index-1]){
+                    int temp = arr[index-1];
+                    arr[index-1] = arr[index];
+                    arr[index] = temp;
+                    innerfound = true;
+                    continue;
+                }
+            }
+            found = innerfound;
+        }
+    }
+
+    public void segregateEvenOdd(int[] arr){
+        sortArray(arr);
+        int leftPtr = 0, rightPtr = 0;
+        while(leftPtr < arr.length && rightPtr < arr.length){
+            if (isEven(arr[leftPtr])){
+                if(leftPtr < arr.length - 1) leftPtr++;
+                if (rightPtr < arr.length - 1) rightPtr++;
+            } else {
+                while(! isEven(arr[rightPtr])){
+                    if (rightPtr < arr.length-1) {
+                        rightPtr++;
+                    }
+                    else break;
+                }
+                int temp = arr[leftPtr];
+                arr[leftPtr] = arr[rightPtr];
+                arr[rightPtr] = temp;
+                leftPtr++;
+                rightPtr++;
+            }
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//{ Driver Code Starts
+
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        Scanner scanner = new Scanner(System.in);
+//        int t = Integer.parseInt(scanner.nextLine());
+//
+//        Solution ob = new Solution();
+//
+//        while (t-- > 0) {
+//            String line = scanner.nextLine();
+//            Scanner lineScanner = new Scanner(line);
+//            List<Integer> nums = new ArrayList<>();
+//            while (lineScanner.hasNextInt()) {
+//                nums.add(lineScanner.nextInt());
+//            }
+//            System.out.println(ob.maxTripletSum(nums));
+//        }
+//        scanner.close();
+        Solution solution = new Solution();
+        int[] nums = {5,2,3,4,1};
+        System.out.println(solution.maxTripletSum(Arrays.stream(nums).boxed().toList()));
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find the maximum triplet sum in the array.
+    public int maxTripletSum(List<Integer> arr) {
+        // Complete the function
+
+        // GETTING POSSIBLE COMBINATIONS ON INDICES
+        List<Integer[]> possible_indices = new ArrayList<>(List.of());
+        for (Integer i : IntStream.range(0, arr.size()).toArray()){
+            for (Integer j : IntStream.range(i+1, arr.size()).toArray()){
+                for (Integer k : IntStream.range(j+1, arr.size()).toArray()){
+                    Integer[] thisList = {i, j, k};
+                    if(!possible_indices.contains(thisList)){
+                        possible_indices.add(thisList);
+                    }
+                }
+            }
         }
+
+        List<Integer> sums = new ArrayList<>(List.of());
+
+        for (Integer[] ind_set : possible_indices){
+            int sum = Arrays.stream(ind_set).toList().stream().mapToInt(arr::get).sum();
+            sums.add(sum);
+        }
+
+        if(!sums.isEmpty()){
+            return Collections.max(sums);
+        }
+
+        return 0;
     }
 }
